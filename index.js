3b4c5d1180e0b60149e6c54dedac14cdf6d24e00
Zev <zevgeller@gmail.com>
2022-06-01 18:57:53 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Write your code here!
+const ul = document.querySelector("main");
+
+ul.remove("main");
+
+//const li = document.createElement("newHeader");
+
+const newHeader = document.createElement("h1");
+
+
+newHeader.id = 'victory';
+newHeader.innerHTML = "<h1>Z is the champion</h1>";
+
+document.body.append(newHeader);
+//newHeader.id(`victory`);
+
+
+//= "h1#victory";
+//newHeader.id = 
+//  
